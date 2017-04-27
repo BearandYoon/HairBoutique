d1edeb859d74a44a565fdbfba9f99b5d7dcb14bf
mac <luckysevenbear@gmail.com>
2017-04-27 12:42:30 +0300

    added home component

---
+++ imports/ui/pages/home/home.js
@@ -0,0 +1,19 @@
+import './home.html';
+
+Template.home.helpers({
+
+    // For demo purpose
+    // Example of notification list
+    notifications : [
+        {number: 1, labelClass: 'success', content: 'Please contact me', time: '09:00 pm'},
+        {number: 2, labelClass: 'info', content: 'Sign a contract', time: '10:16 am'},
+        {number: 3, labelClass: 'primary', content: 'Open new shop', time: '08.22 pm'},
+        {number: 4, labelClass: 'default', content: 'Call back to Sylvia', time: '11:06 pm'},
+        {number: 5, labelClass: 'primary', content: 'Write a letter to Sandra', time: '12:00 pm'}
+    ]
+
+});
+
+Template.home.rendered = function(){
+
+};
