b74a404babb97ffdeb51e3b5bcbe37e1d61d04f7
BearandYoon <luckysevenbear@gmail.com>
2017-10-27 19:07:24 +0200

    display my articles

--- imports/ui/pages/home/home.js
+++ imports/ui/pages/home/home.js
@@ -7,26 +7,33 @@ import { updateArticle } from '../../../api/articles/methods.js';
 import { Articles } from "../../../api/articles/articles";
 import { Modal } from 'meteor/peppelg:bootstrap-3-modal';
 
+const monthNames = [
+    'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
+];
+
 Template.home.onCreated(function homePageOnCreated() {
     console.log('homepage.onCreated');
     this.state = new ReactiveDict();
     window.scrollTo(0, 0);
+    this.autorun(() => {
+        if (this.subscriptionsReady()) {
+            this.state.set('articles', Articles.find({authorId: Meteor.userId()}).fetch());
+        }
+    });
 });
 
 Template.home.rendered = function(){
     $('.ladda-button').ladda();
-
-    let msnry = new Masonry( '.grid', {
-        itemSelector: '.grid-item',
-        columnWidth: 300,
-        gutter: 25
-    });
 };
 
 Template.home.onRendered(function homePageOnRendered() {
     this.autorun(() => {
         if (this.subscriptionsReady()) {
-            this.state.set('articles', Articles.find().fetch());
+            let msnry = new Masonry( '.grid', {
+                itemSelector: '.grid-item',
+                columnWidth: 500,
+                gutter: 25
+            });
         }
     });
 });
@@ -36,7 +43,15 @@ Template.home.events({});
 Template.home.helpers({
     articles() {
         const instance = Template.instance();
-        console.log('articles = ', instance.state.get('articles'));
-        return instance.state.get('articles');
+        let myArticles = instance.state.get('articles');
+        if (myArticles) {
+            myArticles.map(function (article) {
+                article.formatDate = monthNames[article.createdAt.getMonth()] + " " + article.createdAt.getDate() + ", " + article.createdAt.getFullYear();
+            })
+        }
+        return myArticles;
+    },
+    userInfo() {
+        return Meteor.user();
     }
 });
